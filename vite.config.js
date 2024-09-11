a34cad962073d8c16675d21a79282ae14d68c58c
Norman Lumilaan <74055371+normanlumilaan@users.noreply.github.com>
2024-09-11 03:52:26 +0300

    feat: add templating (#1)
    * build: add vite-plugin-ejs v1.7.0
    
    * refactor: remove orphan pug code
    
    * feat: set up EJS template and data object
    
    * feat: templates and data object
    
    * refactor!: change page data and files structureI

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig, loadEnv } from "vite";
-import { fileURLToPath } from "node:url";
 import path from "node:path";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { getData } from "./src/data/data";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+//import { fileURLToPath } from "node:url";
+//const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   const port = parseInt(env.VITE_APP_PORT, 10) || undefined;
-
-  // Home url with port number
-  if (port) {
-    process.env.VITE_APP_HOME_URL = `${env.VITE_APP_HOME_URL}:${port}`;
-  }
+  const url = env.VITE_APP_HOME_URL;
+  const homeUrl = mode === "development" ? `${url}:${port}` : url;
 
   return {
     base: "/",
@@ -27,5 +26,9 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
+    plugins: [
+      // With Data
+      ViteEjsPlugin(getData(homeUrl)),
+    ],
   };
 });
