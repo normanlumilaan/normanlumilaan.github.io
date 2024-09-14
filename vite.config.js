bbd4953cb213a49243d8dac42dadf261467b4844
Norman Lumilaan <74055371+normanlumilaan@users.noreply.github.com>
2024-09-14 13:47:11 +0300

    build: minify html (#8)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import path from "node:path";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import { getData } from "./src/data/data";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 //import { fileURLToPath } from "node:url";
 //const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -26,6 +27,6 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    plugins: [ViteEjsPlugin(getData(homeUrl))],
+    plugins: [ViteEjsPlugin(getData(homeUrl)), ViteMinifyPlugin({})],
   };
 });
