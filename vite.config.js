c0e461b3d96077453e473298eb17634a046a8eeb
Norman Lumilaan <norman.lumilaan@gmail.com>
2024-09-17 12:58:24 +0300

    refactor: explicitly define __dirname

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,32 @@
-import { defineConfig, loadEnv } from "vite";
-import path from "node:path";
-import { ViteEjsPlugin } from "vite-plugin-ejs";
-import { getData } from "./src/data/data";
-import { ViteMinifyPlugin } from "vite-plugin-minify";
+import { defineConfig, loadEnv } from 'vite'
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
+import { ViteEjsPlugin } from 'vite-plugin-ejs'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+import { getData } from './src/data/data'
 
-//import { fileURLToPath } from "node:url";
-//const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  const port = parseInt(env.VITE_APP_PORT, 10) || undefined;
-  const url = env.VITE_APP_HOME_URL;
-  const homeUrl = mode === "development" ? `${url}:${port}` : url;
+  const env = loadEnv(mode, process.cwd())
+  const port = parseInt(env.VITE_APP_PORT, 10) || undefined
+  const url = env.VITE_APP_HOME_URL
+  const homeUrl = mode === 'development' ? `${url}:${port}` : url
 
   return {
-    base: "/",
+    base: '/',
     server: {
       port: port,
-      host: "0.0.0.0",
+      host: '0.0.0.0',
     },
     build: {
       rollupOptions: {
         input: {
-          home: path.resolve(__dirname, "index.html"),
-          page404: path.resolve(__dirname, "404.html"),
+          home: path.resolve(__dirname, 'index.html'),
+          page404: path.resolve(__dirname, '404.html'),
         },
       },
     },
     plugins: [ViteEjsPlugin(getData(homeUrl)), ViteMinifyPlugin({})],
-  };
-});
+  }
+})
